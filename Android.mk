348967d6e75456fa8e1b56220c22d712742b71d0
Kubo Ryosuke <kubo_ryosuke@cyberagent.co.jp>
2014-01-21 21:53:20 +0900

    Makfile (sync to cocos2d-x 3.0 beta)

--- Android.mk
+++ Android.mk
@@ -1,4 +1,3 @@
-
-COCONUT_SRC_FILES := $(shell pushd $(LOCAL_PATH) 1>&2;/bin/find ../../Libraries/coconut/coconut -name '*.cpp'; popd 1>&2)
-
+COCONUT_SRC_FILES := $(shell find $(LOCAL_PATH)/../../Libraries/coconut/coconut -name '*.cpp') \
+                     $(shell find $(LOCAL_PATH)/../../Libraries/coconut/coconut -name '*.c')
 COCONUT_C_INCLUDES := $(LOCAL_PATH)/../../Libraries/coconut
